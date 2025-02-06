2221d60533a2f086d8059084c1e3c501178b81f7
morriganlovekarrigan <ergashevmaksud9@gmail.com>
2025-02-06 15:06:26 +0500

    refactor: App into class-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
-function App() {
-  return <div></div>;
+import { Component } from 'react';
+import ErrorBoundary from './components/error/boundary';
+import HomePage from './pages/home';
+
+class App extends Component {
+  render() {
+    return (
+      <ErrorBoundary>
+        <HomePage />
+      </ErrorBoundary>
+    );
+  }
 }
+
 export default App;
