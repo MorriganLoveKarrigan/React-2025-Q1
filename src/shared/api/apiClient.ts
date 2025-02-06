95f2b2d6b36007560d94325b901ddcf709ef289c
morriganlovekarrigan <ergashevmaksud9@gmail.com>
2025-02-06 15:16:49 +0500

    refactor: remove access token

--- src/shared/api/apiClient.ts
+++ src/shared/api/apiClient.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const defaultHeaders = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
-  Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
 };
 
 export const apiClient = axios.create({
